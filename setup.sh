53ad60452816b2536354c1e2c80b95734d87896c
Dave Martorana <dave@flyclops.com>
2023-08-15 21:23:10 -0400

    chsh all the time

--- setup.sh
+++ setup.sh
@@ -5,28 +5,25 @@ set -e
 echo "Setting up via dotfiles..."
 
 CHECKFILE="$HOME/.dotfiles/initial-setup-done"
-if [ -f $CHECKFILE ]; then
-    echo "Initial setup already done"
-    exit 0
+if [ ! -f $CHECKFILE ]; then
+    # Make directories
+    mkdir -p $HOME/bin
+    mkdir -p $HOME/code
+    cp -rv .dotfiles $HOME
+
+    # Run zsh/setup.sh
+    source $HOME/.dotfiles/zsh/setup.sh
+
+    # Run fzf/setup.sh
+    source $HOME/.dotfiles/fzf/setup.sh
+
+    # Install antibody
+    source $HOME/.dotfiles/antibody/setup.sh
+
+    # Make sure we don't do it again
+    touch $CHECKFILE
 fi
-
-# Make directories
-mkdir -p $HOME/bin
-mkdir -p $HOME/code
-cp -rv .dotfiles $HOME
-
-# Run zsh/setup.sh
-source $HOME/.dotfiles/zsh/setup.sh
-
-# Run fzf/setup.sh
-source $HOME/.dotfiles/fzf/setup.sh
-
-# Install antibody
-source $HOME/.dotfiles/antibody/setup.sh
 
 # Change shell
 ZSH=$(which zsh)
 sudo chsh -s $(which zsh) $(whoami) # https://askubuntu.com/a/1325754
-
-# Make sure we don't do it again
-touch $CHECKFILE
