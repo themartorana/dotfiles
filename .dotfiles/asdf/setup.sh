b0f7aae926212217832622dc6dfb6f2e334121ac
Dave Martorana <dave@flyclops.com>
2023-08-17 17:03:31 -0400

    Found chmod issue

--- .dotfiles/asdf/setup.sh
+++ .dotfiles/asdf/setup.sh
@@ -6,11 +6,12 @@ then
     # install asdf/nano
     git config --global advice.detachedHead false
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
-    chmod 755 .asdf/asdf.sh
+    chmod u+x ~/.asdf/asdf.sh
+    chmod u+x ~/.asdf/asdf.zsh
+    chmod u+x ~/.asdf/asdf.bash
     echo ". \"\$HOME/.asdf/asdf.sh\"" >> ~/.bashrc
     echo ". \"\$HOME/.asdf/completions/asdf.bash\"" >> ~/.bashrc
     echo ". \"\$HOME/.asdf/asdf.sh\"" >> ~/.zshrc
     echo ". \"\$HOME/.asdf/completions/asdf.zsh\"" >> ~/.zshrc
-    echo "Echos done"
     source ~/.bashrc
 fi
