0cbb4f928ce1eda8490a6ea85511d16bbddf31e5
Dave Martorana <dave@flyclops.com>
2023-08-17 17:20:06 -0400

    Reduce asdf attempts

--- .dotfiles/asdf/setup.sh
+++ .dotfiles/asdf/setup.sh
@@ -7,8 +7,6 @@ then
     git config --global advice.detachedHead false
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
     chmod u+x ~/.asdf/asdf.sh
-    chmod u+x ~/.asdf/asdf.bash
     echo ". \"\$HOME/.asdf/asdf.sh\"" >> ~/.bashrc
-    echo ". \"\$HOME/.asdf/completions/asdf.bash\"" >> ~/.bashrc
     source ~/.bashrc
 fi
