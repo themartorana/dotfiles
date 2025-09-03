ae1c04286035fccceaad622c052942dfc052fc61
Dave Martorana <themartorana@users.noreply.github.com>
2025-09-03 03:30:44 +0000

    Trying again and again

--- .dotfiles/claude/setup.sh
+++ .dotfiles/claude/setup.sh
@@ -7,7 +7,12 @@ sudo ln -s "$HOME/.local/bin/claude" "/usr/local/bin/claude"
 # Necessary, frustratingly 
 # See: https://github.com/anthropics/claude-code/issues/1084
 printf "\n\nexport ANTHROPIC_AUTH_TOKEN=\"hello-world\"\n" >> $HOME/.profile
+export ANTHROPIC_AUTH_TOKEN="hello-world"
 
 # Set up claude run-once
-sudo chmod +x $HOME/.dotfiles/claude/set-perms.sh
-sudo su -c "$HOME/.dotfiles/claude/set-perms.sh" root
+echo "Setting theme..."
+claude config set -g theme dark
+echo "Setting trust dialog..."
+claude config set hasTrustDialogAccepted true
+echo "Setting project onboarding..."
+claude config set hasCompletedProjectOnboarding true
