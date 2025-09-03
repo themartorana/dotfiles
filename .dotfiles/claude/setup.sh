27beef71a1f2f5b7be72b533daf572bf713065e4
Dave Martorana <themartorana@users.noreply.github.com>
2025-09-03 02:54:47 +0000

    New try

--- .dotfiles/claude/setup.sh
+++ .dotfiles/claude/setup.sh
@@ -6,11 +6,11 @@ sudo ln -s "$HOME/.local/bin/claude" "/usr/local/bin/claude"
 
 # Necessary, frustratingly 
 # See: https://github.com/anthropics/claude-code/issues/1084
-printf "\n\nexport ANTHROPIC_AUTH_TOKEN=\"hello-world\"\n" >> $HOME/.zshrc
+printf "\n\nexport ANTHROPIC_AUTH_TOKEN=\"hello-world\"\n" >> $HOME/.profile
 
 # Set dark theme
-claude config set -g theme dark
-claude config set hasTrustDialogAccepted true
-claude config set hasCompletedProjectOnboarding true
+zsh -c 'claude config set -g theme dark'
+zsh -c 'claude config set hasTrustDialogAccepted true'
+zsh -c 'claude config set hasCompletedProjectOnboarding true'
 
-claude config ls
+bash -c 'claude config ls'
