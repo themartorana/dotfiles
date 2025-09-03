75d0a0fd3d86501315178a650f1fe30f2817aa4c
Dave Martorana <themartorana@users.noreply.github.com>
2025-09-03 02:48:08 +0000

    Another setting

--- .dotfiles/claude/setup.sh
+++ .dotfiles/claude/setup.sh
@@ -9,5 +9,6 @@ sudo ln -s "$HOME/.local/bin/claude" "/usr/local/bin/claude"
 printf "\n\nexport ANTHROPIC_AUTH_TOKEN=\"hello-world\"\n" >> $HOME/.zshrc
 
 # Set dark theme
-claude config set -g theme dark
+claude config set theme dark
 claude config set hasTrustDialogAccepted true
+claude config set hasCompletedProjectOnboarding true
