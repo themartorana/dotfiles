2b087d73b72c787017ab7c3629d8f789aa0d30d4
Dave Martorana <themartorana@users.noreply.github.com>
2025-09-03 02:16:39 +0000

    claude headless progress

--- .dotfiles/claude/setup.sh
+++ .dotfiles/claude/setup.sh
@@ -2,6 +2,12 @@
 
 # Install claude
 curl -fsSL https://claude.ai/install.sh | bash
+ln -s "$HOME/.local/bin/claude" "/usr/local/bin/claude"
+
+# Necessary, frustratingly 
+# See: https://github.com/anthropics/claude-code/issues/1084
+echo "\nexport ANTHROPIC_AUTH_TOKEN=\"hello-world\"" >> $HOME/.zshrc
 
 # Set dark theme
 claude config set -g theme dark
+claude config set hasTrustDialogAccepted true
