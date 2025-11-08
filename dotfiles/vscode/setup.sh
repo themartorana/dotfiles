c72ab9cfb8fcf6162bf41837938bd5ee2d23abad
Dave Martorana <themartorana@yahoo.com>
2025-11-08 17:31:16 +0000

    Turn off settings, add extensions

--- dotfiles/vscode/setup.sh
+++ dotfiles/vscode/setup.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-# Create the directory if it doesn't exist
-mkdir -p /home/vscode/.vscode-server/data/Machine
+# # Create the directory if it doesn't exist
+# mkdir -p /home/vscode/.vscode-server/data/Machine
 
-# Path to the settings file
-SETTINGS_FILE="/home/vscode/.vscode-server/data/Machine/settings.json"
+# # Path to the settings file
+# SETTINGS_FILE="/home/vscode/.vscode-server/data/Machine/settings.json"
 
-function apply_setting() {
-    local key="$1"
-    local value="$2"
-    if [ ! -f "$SETTINGS_FILE" ]; then
-        # Create a new settings file with the required setting
-        jq -n --arg k "$key" --arg v "$value" '{($k): $v}' > "$SETTINGS_FILE"
-    else
-        # Add or update the setting in the existing file
-        if jq -e --arg k "$key" 'has($k)' "$SETTINGS_FILE" >/dev/null; then
-            # Update the existing setting
-            jq --arg k "$key" --arg v "$value" '.[$k] = $v' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
-        else
-            # Add the new setting
-            jq --arg k "$key" --arg v "$value" '. + {($k): $v}' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
-        fi
-    fi
-}
+# function apply_setting() {
+#     local key="$1"
+#     local value="$2"
+#     if [ ! -f "$SETTINGS_FILE" ]; then
+#         # Create a new settings file with the required setting
+#         jq -n --arg k "$key" --arg v "$value" '{($k): $v}' > "$SETTINGS_FILE"
+#     else
+#         # Add or update the setting in the existing file
+#         if jq -e --arg k "$key" 'has($k)' "$SETTINGS_FILE" >/dev/null; then
+#             # Update the existing setting
+#             jq --arg k "$key" --arg v "$value" '.[$k] = $v' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
+#         else
+#             # Add the new setting
+#             jq --arg k "$key" --arg v "$value" '. + {($k): $v}' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
+#         fi
+#     fi
+# }
 
-# Check if the settings file exists
-apply_setting "terminal.integrated.defaultProfile.linux" "zsh"
+# # Check if the settings file exists
+# apply_setting "terminal.integrated.defaultProfile.linux" "zsh"
+
+# Install extensions
+code --install-extension letmaik.git-tree-compare
+code --install-extension anthropic.claude-code
+code --install-extension vivaxy.vscode-conventional-commits
+code --install-extension onatm.open-in-new-window
+code --install-extension redhat.vscode-yaml
+
