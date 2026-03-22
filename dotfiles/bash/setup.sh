b96049400448b945eefb894189811a539d20ba49
Dave Martorana <themartorana@yahoo.com>
2026-03-22 22:42:00 +0000

    fix: bash locals

--- dotfiles/bash/setup.sh
+++ dotfiles/bash/setup.sh
@@ -4,11 +4,12 @@ set -e
 # copy .bash_profile
 [ -f ~/.bash_profile ] && mv ~/.bash_profile ~/.bash_profile.bak
 ln -s ~/.dotfiles/dotfiles/bash/bash_profile ~/.bash_profile
+[ ! -f ~/.bash_profile.local ] && cp ~/.dotfiles/dotfiles/bash/bash_profile.local ~/.bash_profile.local
 
 # copy .bashrc
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
 ln -s ~/.dotfiles/dotfiles/bash/bashrc ~/.bashrc
-[ ! -f ~/.bashrc.local ] && ln -s ~/.dotfiles/dotfiles/bash/bashrc.local ~/.bashrc.local
+[ ! -f ~/.bashrc.local ] && cp ~/.dotfiles/dotfiles/bash/bashrc.local ~/.bashrc.local
 
 # Set up the .bash_history file
 [ -f ~/.bash_history ] || touch ~/.bash_history
